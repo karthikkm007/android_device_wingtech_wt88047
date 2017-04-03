a0cf87a79af797f7ce567bdc4ba5fc678f7437a2
ssawrav <s.sawrav@gmail.com>
2017-04-03 10:31:55 +0530

    Initial 64 bit prep

--- cm.mk
+++ cm.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
