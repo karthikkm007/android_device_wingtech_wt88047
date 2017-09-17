a72db758523737201e97d7ed7dc0d7fb9b7985b3
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    msm8916-common: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- device.mk
+++ device.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.cyanogen_8916
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
