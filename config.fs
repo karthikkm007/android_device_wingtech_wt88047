e00380d2513e7bdfefeefbb936f2b1b291400529
MkfsSion <myychina28759@gmail.com>
2018-06-28 10:42:26 +0800

    wt88047: Update config_fs.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- config.fs
+++ config.fs
@@ -1,14 +1,5 @@
-[system/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
-
-[AID_QCOM_DIAG]
-value:2950
-
-[AID_RFS]
-value:2951
-
-[AID_RFS_SHARED]
-value:2952
