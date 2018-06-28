9416042efbcedd9d80c4e0cf60a81bddd925bfbe
MkfsSion <myychina28759@gmail.com>
2018-06-28 12:30:13 +0800

    wt88047: Remove lineage overlay.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- device.mk
+++ device.mk
@@ -230,8 +230,7 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
