fa2feaceb2b28abfa1ad3b2bdc7c1c9257b692f9
MkfsSion <myychina28759@gmail.com>
2018-06-28 09:59:25 +0800

    wt88047: Drop shim for mm-qcamera-daemon.
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,6 @@ include device/qcom/sepolicy/legacy-sepolicy.mk
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
     /system/vendor/lib/libflp.so|libshims_flp.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
     /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
