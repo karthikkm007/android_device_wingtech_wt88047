9a0cf00ceec21d31fb20a818a7376253f1fbbbb3
Nicholas Lim <nicknitewolf@lineageos.org>
2018-07-04 14:30:33 +0800

    wt88047: Remove healthd suspend
     * Doesn't actually work properly, display remains
       on even when screen blanks out.
     * There is no longer much benefit to suspending the
       charging animation after a while of inactivity.
    
    Change-Id: I24f34df3fb9487fa27209909a616b92819e9c21f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,9 +90,7 @@ TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
 # Healthd
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8916
