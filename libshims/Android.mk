8c7cfa7a37d44fae1a77a04a3d7d066784ec0d60
nicknitewolf <nicknitewolf@lineageos.org>
2018-07-09 11:13:55 +0530

    wt88047: kill IMS shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -46,12 +46,3 @@ LOCAL_MODULE := libshims_get_process_name
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := MediaBuffer.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshims_ims
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
