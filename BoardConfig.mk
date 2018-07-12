cf2b7e2915840835c2526e2aa0d0dc8115536402
karthikkm007 <jacksparrow941994@gmail.com>
2018-07-12 19:09:36 +0530

    Revert "wt88047: Bring back our custom libhealthd"
    This reverts commit 6eeb01c96c076bf20c6189daf7ebd515ebfa1267.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,8 +103,9 @@ TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
 # Healthd
+BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.wt88047
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8916
