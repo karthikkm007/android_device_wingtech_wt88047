8fa2f371a884b42c2f8f4ae6ca5d53ad05056228
karthikkm007 <jacksparrow941994@gmail.com>
2018-07-28 07:33:57 +0530

    Revert "wt88047: vendor path back to wingtech"
    This reverts commit 77285cf33c7e1aa77dd36aa9a2093f0c23c1b280.

--- aosp_wt88047.mk
+++ aosp_wt88047.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/wingtech/wt88047/wt88047-vendor.mk)
+$(call inherit-product, vendor/xiaomi/wt88047/wt88047-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from those products. Most specific first.
