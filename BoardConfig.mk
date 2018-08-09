03434633ebda19fdc0330a362cfa1e202cec1598
MkfsSion <myychina28759@gmail.com>
2018-08-09 22:35:37 +0800

    wt88047: Get rid of recovery only fstab
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,7 +150,7 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.full
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 BOARD_NO_SECURE_DISCARD := true
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cm
 
