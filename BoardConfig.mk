4b8559776c56cfdee99b399da98e112d55cdd2a4
karthikkm007 <jacksparrow941994@gmail.com>
2018-10-01 10:20:41 +0000

    wt88047: deodex rom
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,7 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 ifeq ($(HOST_OS),linux)
     ifneq ($(TARGET_BUILD_VARIANT),eng)
         ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
+            WITH_DEXPREOPT := false
             WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
         endif
     endif
