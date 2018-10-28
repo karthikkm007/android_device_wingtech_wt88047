73263bb791e23583a664cda9c7d46506ca301a23
karthikkm007 <jacksparrow941994@gmail.com>
2018-10-28 19:19:02 +0530

    wt88047: bring back graphics composer, allocator services and copybit
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- device.mk
+++ device.mk
@@ -97,9 +97,13 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    copybit.msm8916 \
     gralloc.msm8916 \
     hwcomposer.msm8916 \
     libgenlock \
