157592486ed1dac1b6e2a51dc8c111777872f805
Santhosh Kumar Thimmanna Bhattar <sthim@codeaurora.org>
2018-12-18 11:38:39 +0530

    Camera: Change in manifests xml file
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- device.mk
+++ device.mk
@@ -246,6 +246,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.msm8916 \
     libmm-qcamera \
