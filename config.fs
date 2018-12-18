5d0ab76f1046c958f3204f97c1c7fab339892c75
Ad!thya R <adithya.r02@outlook.com>
2018-12-18 17:25:37 +0530

    wt88047: Update config.fs

--- config.fs
+++ config.fs
@@ -1,11 +1,41 @@
-[system/vendor/bin/imsdatadaemon]
+[AID_VENDOR_QTI_DIAG]
+value:2901
+
+[AID_VENDOR_QCOM_DIAG]
+value:2950
+
+[AID_VENDOR_RFS]
+value:2951
+
+[AID_VENDOR_RFS_SHARED]
+value:2952
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/imsdatadaemon]
 mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/imsdatadaemon]
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[system/vendor/bin/cnd]
+mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-mode: 0755
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
