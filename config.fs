1f87ea11fa95f87fa01813bab0845303c238c7b7
karthik km <jacksparrow941994@gmail.com>
2018-12-18 17:36:08 +0530

    remove unused stuffs

--- config.fs
+++ config.fs
@@ -10,12 +10,6 @@ value:2951
 [AID_VENDOR_RFS_SHARED]
 value:2952
 
-[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: BLOCK_SUSPEND NET_ADMIN
-
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
@@ -33,9 +27,3 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
-
-[system/vendor/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
