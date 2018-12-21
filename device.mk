04be33c1d3fa560890bcb82e304504f99d290d33
karthikkm007 <jacksparrow941994@gmail.com>
2018-12-21 22:10:43 +0530

    Revert "wt88047: update audio HALs to 4.0"
    This reverts commit 5ea836cb22b6da1da3cba1ab9d1483242c0853a4.
    
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- device.mk
+++ device.mk
@@ -131,13 +131,11 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libqcomvoiceprocessingdescriptors \
     libqcompostprocbundle\
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio@4.0-service \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@4.0-service \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    android.hardware.soundtrigger@2.1-service
+    android.hardware.soundtrigger@2.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
