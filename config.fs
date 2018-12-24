53bdd081463eb44d29de907317668a466da63430
karthikkm007 <jacksparrow941994@gmail.com>
2018-12-24 13:58:39 +0530

    ims stuff <<<<< Bring back  >>>>>
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- config.fs
+++ config.fs
@@ -1,29 +1,5 @@
-[AID_VENDOR_QTI_DIAG]
-value:2901
-
-[AID_VENDOR_QCOM_DIAG]
-value:2950
-
-[AID_VENDOR_RFS]
-value:2951
-
-[AID_VENDOR_RFS_SHARED]
-value:2952
-
-[system/vendor/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
-
-[system/vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/imsrcsd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
