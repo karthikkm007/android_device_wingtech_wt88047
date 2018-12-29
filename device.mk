544520c5d171283c8e2038aa1c47346c8976ce78
karthikkm007 <jacksparrow941994@gmail.com>
2018-12-29 13:13:51 +0530

    wt88047: lets whitelist ims services
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/qti_whitelist.xml
 
 # Init scripts
 PRODUCT_PACKAGES += \
