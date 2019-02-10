e0969f13ff31a433038820596129576762305cb9
karthikkm007 <jacksparrow941994@gmail.com>
2019-02-10 16:26:19 +0530

    wt88047: shim camera-daemon
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,9 +167,10 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
+    /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
     /system/vendor/lib/libflp.so|libshims_flp.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
-    /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so 
+    /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
 
 # Wlan
 BOARD_HAS_QCOM_WLAN := true
