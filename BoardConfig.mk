f6d85d6ffc2666f89ba57b62fafc86edf9974f81
karthikkm007 <jacksparrow941994@gmail.com>
2019-12-11 05:57:25 +0530

    wt88047: Re-enable sepolices
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,10 +166,10 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cm
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
-#BOARD_SEPOLICY_DIRS += \
-#    $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy
 
-#include device/qcom/sepolicy-legacy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/temp
 
